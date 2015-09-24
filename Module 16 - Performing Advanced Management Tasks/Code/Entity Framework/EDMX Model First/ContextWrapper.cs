ea30ae6f744656a906a05c4d0fad09b81db8e4eb
Jeff McBride <jeff@geeklearn.com>
2015-09-24 06:52:37 -0400

    More

---
+++ Module 16 - Performing Advanced Management Tasks/Code/Entity Framework/EDMX Model First/ContextWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDMX_Model_First
+{
+    public static class ContextWrapper
+    {
+        private static CustomerContextContainer context = new CustomerContextContainer();
+        public static List<Customer> getCustomers() 
+        { 
+            return context.Customers.ToList<Customer>();
+        }
+
+        public static void addCustomer(Customer newCustomer)
+        {
+            context.Customers.Add(newCustomer);
+        }
+    }
+}
